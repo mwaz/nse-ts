2edeed1b724cc74f674e73416aaa06eccafd34b6
mwaz <waweru.mwaura@allangray.co.za>
2020-03-06 06:34:23 +0300

    fix: typescript initialization

---
+++ index.ts
@@ -0,0 +1,19 @@
+// const siteUrl = "https://onlinetrading.ncbagroup.com/Tradeweb/login.aspx";
+// const axios = require("axios");
+// const cheerio = require("cheerio");
+
+
+// const fetchData = async () => {
+//   const result = await axios.get(siteUrl);
+//   return cheerio.load(result.data);
+// };
+
+
+
+// const getResults = async () => {
+//     const $ = await fetchData();
+//     const loginButton = $('#btnLogin').text();
+//     console.log(await cheerio.load(result.data)) // Logs 'Post a Job'
+// }
+
+// getResults()
