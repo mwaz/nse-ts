7a83d59b0752e32793b62402d99eaf582dc0d298
mwaz <amby.mwaz@gmail.com>
2020-03-16 17:09:00 +0300

    fix: app refactor

--- src/data/marketRequest.ts
+++ src/data/marketRequest.ts
@@ -1,15 +1,46 @@
+import * as fs from 'fs';
 import  * as puppeteer from 'puppeteer';
 
-(async () => {
-    const browser = await puppeteer.launch({defaultViewport: { width: 1024, height: 720 }, args: ['--start-fullscreen', '--window-size=1920,1040']});
-    const page = await browser.newPage();
+let browser; 
+let page; 
+
+export const appLogin = async() => {
+    browser = await puppeteer.launch({defaultViewport: { width: 1024, height: 720 }, args: ['--start-fullscreen', '--window-size=1920,1040']});
+    page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 1040 });
     await page.goto('https://onlinetrading.ncbagroup.com/Tradeweb/login.aspx');
     await page.$('#txtLogin');
-    await page.type('#txtLogin', 'WAWERU');
-    await page.type('#txtPassword', 'K9q4c4WcacDEZKK');
+    await page.type('#txtLogin', process.env.USERNAME);
+    await page.type('#txtPassword', process.env.PASSWORD);
     await page.click('#btnLogin');
     await page.waitForResponse(response => response.url() === 'https://onlinetrading.ncbagroup.com/Tradeweb/lareq.aspx' && response.status() === 200);
+}
+
+export const writeData = async(path, data,) => {
+    await fs.writeFile(path, data, err => {
+        err ?  console.log('Error writing file', err) :  console.log('Successfully wrote file')
+    });
+}
+
+export const getIframeData = async(page) => {
+    const frame = await page.frames().find(frame => frame.name() === 'iMarketDetailsFrame');
+    let optionsResult = [];
+    let resultsObject = [];
+
+    for(let i=1; i<60; i++){
+        optionsResult = await frame.$$eval(`#trMW_${i}`, (options:any) => options.map(option => option.textContent));
+        await resultsObject.push(optionsResult);
+    }
+    return resultsObject;
+}
+
+const fetchData = (async () => {
+    await appLogin();
+    const resultsObject = await getIframeData(page);
+    await writeData('./nse.json', resultsObject);
     await page.screenshot({path: 'screenshot.png'});
     await browser.close();
+    await console.log(resultsObject);
   })();
+
+  export default fetchData;
--- src/routes/markets.ts
+++ src/routes/markets.ts
@@ -1,8 +1,8 @@
 import { Router, Request, Response, NextFunction} from 'express';
 // const Markets = require('../data/marketRequest');
 import  * as puppeteer from 'puppeteer';
-import * as fs from 'fs';
 
+import fetchData from '../data/marketRequest';
 
 export class MarketsRouter {
   router: Router
@@ -19,38 +19,7 @@ export class MarketsRouter {
    * GET all Markets.
    */
   public getAll(req, res) {
-    return (async () => {
-        const browser = await puppeteer.launch({defaultViewport: { width: 1024, height: 720 }, args: ['--start-fullscreen', '--window-size=1920,1040']});
-        const page = await browser.newPage();
-        await page.setViewport({ width: 1920, height: 1040 });
-        await page.goto('https://onlinetrading.ncbagroup.com/Tradeweb/login.aspx');
-        await page.$('#txtLogin');
-        await page.type('#txtLogin', process.env.USERNAME);
-        await page.type('#txtPassword', process.env.PASSWORD);
-        await page.click('#btnLogin');
-        await page.waitForResponse(response => response.url() === 'https://onlinetrading.ncbagroup.com/Tradeweb/lareq.aspx' && response.status() === 200);
-        const frame = await page.frames().find(frame => frame.name() === 'iMarketDetailsFrame');
-        let optionsResult = [];
-        let resulstsObject = [];
-
-        for(let i=1; i<60; i++){
-            optionsResult = await frame.$$eval(`#trMW_${i}`, (options:any) => options.map(option => option.textContent) );
-            await resulstsObject.push(optionsResult);
-            // await console.log(optionsResult);
-        }
-        await fs.writeFile('./nse.json', resulstsObject, err => {
-            if (err) {
-                console.log('Error writing file', err)
-            } else {
-                console.log('Successfully wrote file')
-            }
-        });
-        
-
-        await page.screenshot({path: 'screenshot.png'});
-        await browser.close();
-        await console.log(resulstsObject);
-      })();
+    return fetchData;
   }
 
   /**
