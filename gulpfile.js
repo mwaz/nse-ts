0616caec0cf0892f6b759a20aa90e06cfa1b758f
mwaz <amby.mwaz@gmail.com>
2020-04-21 17:22:42 +0300

    fix: async functions in yaml gulp runs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const JSON_FILES = ['src/*.json', 'src/**/*.json'];
+const YAML_FILES = ['src/*.yaml', 'src/**/*.yaml'];
+
+const assetFiles = [JSON_FILES, YAML_FILES];
+
 const tsProject = ts.createProject('tsConfig.json');
 function scripts() {
     const tsResult = tsProject.src()
@@ -11,12 +15,17 @@ function watch() {
     gulp.watch('src/**/*.ts', ['scripts']);
 
 }
-function assets() {
-    return gulp.src(JSON_FILES)
+async function assets() {
+    assetFiles.forEach(async asset => {
+        return await gulp.src(asset)
         .pipe(gulp.dest('dist'));
-
+        
+    });
+    // return gulp.src(YAML_FILES )
+    //     // gulp.src(JSON_FILES)
+    //     .pipe(gulp.dest('dist'));
 }
-const build = gulp.series(gulp.parallel(scripts, assets));
+const build = gulp.series(scripts, assets);
 
 exports.assets = assets;
 exports.scripts = scripts;
